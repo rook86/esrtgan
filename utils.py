42d7bf3a6165c3bf12669874dd9e5c971a328059
rook86 <taidem67@gmail.com>
2022-06-30 22:03:15 +0900

    wip

---
+++ utils.py
@@ -0,0 +1,83 @@
+import collections.abc
+import math
+import torch
+import torchvision
+import warnings
+from distutils.version import LooseVersion
+from itertools import repeat
+from torch import nn as nn
+from torch.nn import functional as F
+from torch.nn import init as init
+from torch.nn.modules.batchnorm import _BatchNorm
+
+import numpy as np
+import os
+from collections import OrderedDict
+
+import time
+
+
+@torch.no_grad()
+def default_init_weights(module_list, scale=1, bias_fill=0, **kwargs):
+    if not isinstance(module_list, list):
+        module_list = [module_list]
+    for module in module_list:
+        for m in module.modules():
+            if isinstance(m, nn.Conv2d):
+                init.kaiming_normal_(m.weight, **kwargs)
+                m.weight.data *= scale
+                if m.bias is not None:
+                    m.bias.data.fill_(bias_fill)
+            elif isinstance(m, nn.Linear):
+                init.kaiming_normal_(m.weight, **kwargs)
+                m.weight.data *= scale
+                if m.bias is not None:
+                    m.bias.data.fill_(bias_fill)
+            elif isinstance(m, _BatchNorm):
+                init.constant_(m.weight, 1)
+                if m.bias is not None:
+                    m.bias.data.fill_(bias_fill)
+
+
+def make_layer(basic_block, num_basic_block, **kwarg):
+    layers = []
+    for _ in range(num_basic_block):
+        layers.append(basic_block(**kwarg))
+    return nn.Sequential(*layers)
+
+# TODO: may write a cpp file
+def pixel_unshuffle(x, scale):
+    b, c, hh, hw = x.size()
+    out_channel = c * (scale**2)
+    assert hh % scale == 0 and hw % scale == 0
+    h = hh // scale
+    w = hw // scale
+    x_view = x.view(b, c, h, scale, w, scale)
+    return x_view.permute(0, 1, 3, 5, 2, 4).reshape(b, out_channel, h, w)
+
+def tensor2np(tensor, out_type=np.uint8):
+    num_max = torch.max(tensor).float().cpu().numpy()
+    num_min = torch.min(tensor).float().cpu().numpy()
+    img_np = tensor.float().cpu().detach()[0].numpy()
+    min_max = (num_min, num_max)
+    img_np = (img_np - min_max[0]) / (min_max[1] - min_max[0])  # to range [0, 1]
+    #img_np = np.transpose(img_np, (1, 2, 0))
+    if out_type == np.uint8:
+        img_np = (img_np * 255.0).round()
+
+    return img_np.astype(out_type)
+
+def load_state_dict(path):
+
+    state_dict = torch.load(path)
+    new_state_dcit = OrderedDict()
+    for k, v in state_dict.items():
+        if 'module' in k:
+            name = k[7:]
+        else:
+            name = k
+        new_state_dcit[name] = v
+    return new_state_dcit
+
+
+
